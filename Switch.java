39d4fbd13956b2770731d9ebb66711fdbe01b76f
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-05 10:34:26 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Switch {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int flag=0;
+        if(a>=18)
+        flag=1;
+        switch(flag)
+        {
+            case 0:
+            System.out.println("Not eligible");
+            break;
+            case 1:
+            System.out.println("Eligible");
+            break;
+        }
+    }
+
+    
+}
